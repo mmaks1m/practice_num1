abfec3775351e09f1951a888a267e8c4f2dd3c37
Maksim <demianov.07@gmail.com>
2025-09-09 11:37:50 +0300

    first commit

---
+++ practice1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+//task_1
+//int main(void) {
+//  int num;
+//  scanf("%d", &num);
+//  printf("%d", num);
+//  return 0;
+//}
+
+//task2
+//int main(void){
+//  int num, num2, sum, raz, pr, del;
+//  scanf("%d%d", &num, &num2);
+//  sum = num + num2;
+//  raz = num - num2;
+//  pr = num * num2;
+//  del = num / num2;
+//  printf("sum = %d; difference = %d; multpl = %d; div = %d", sum, raz, pr, del);
+//  return 0;
+//}
+
+//task3
+//int main(void){
+//  int num1, num2, num3;
+//  scanf("%d %d %d", &num1, &num2, &num3);
+//  double avg = (num1+num2+num3) /3.;
+//  printf("%f", avg);
+//  return 0;
+//}
